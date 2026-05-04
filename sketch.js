d0bc3ec732d17e57ec105d9358bb790f8bd4107e
wus28026 <wus28026@gmail.com>
2026-05-04 10:58:45 +0800

    1.3

--- sketch.js
+++ sketch.js
@@ -5,6 +5,10 @@ let faces = [];
 let faceIndices = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
 // 第二組指定的特徵點編號序列 (內嘴唇輪廓)
 let faceIndices2 = [76, 77, 90, 180, 85, 16, 315, 404, 320, 307, 306, 408, 304, 303, 302, 11, 72, 73, 74, 184];
+// 左眼外圈編號序列 (包含 247)
+let eyeOuterIndices = [130, 247, 30, 29, 27, 28, 56, 190, 243, 112, 26, 22, 23, 24, 110, 25];
+// 左眼內圈編號序列 (包含 246)
+let eyeInnerIndices = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246];
 
 function setup() {
   // 建立全螢幕畫布
@@ -44,12 +48,12 @@ function draw() {
     let face = faces[0];
     noFill();
 
-    // 第一組線條：紅色，粗細 15
+    // 第一組線條：紅色，粗細 2 (使其更像口紅的輪廓)
     stroke('red');
-    strokeWeight(15);
-    for (let i = 0; i < faceIndices.length - 1; i++) {
+    strokeWeight(2);
+    for (let i = 0; i < faceIndices.length; i++) {
       let p1 = face.keypoints[faceIndices[i]];
-      let p2 = face.keypoints[faceIndices[i + 1]];
+      let p2 = face.keypoints[faceIndices[(i + 1) % faceIndices.length]];
 
       if (p1 && p2) {
         // 將座標從原始攝影機尺寸縮放到畫布上的 50% 顯示尺寸
@@ -64,12 +68,43 @@ function draw() {
     // 第二組線條：紅色，粗細 1 (您要求的編號序列)
     stroke('red');
     strokeWeight(1);
-    for (let i = 0; i < faceIndices2.length - 1; i++) {
+    for (let i = 0; i < faceIndices2.length; i++) {
       let p1 = face.keypoints[faceIndices2[i]];
-      let p2 = face.keypoints[faceIndices2[i + 1]];
+      let p2 = face.keypoints[faceIndices2[(i + 1) % faceIndices2.length]];
 
       if (p1 && p2) {
         // 座標縮放比例需與影像顯示尺寸 (50%) 一致
+        let x1 = p1.x * (w / capture.width);
+        let y1 = p1.y * (h / capture.height);
+        let x2 = p2.x * (w / capture.width);
+        let y2 = p2.y * (h / capture.height);
+        line(x1, y1, x2, y2);
+      }
+    }
+
+    // 左眼外圈 (包含 247)：紅色，粗細 2
+    stroke('red');
+    strokeWeight(2);
+    for (let i = 0; i < eyeOuterIndices.length; i++) {
+      let p1 = face.keypoints[eyeOuterIndices[i]];
+      let p2 = face.keypoints[eyeOuterIndices[(i + 1) % eyeOuterIndices.length]];
+
+      if (p1 && p2) {
+        let x1 = p1.x * (w / capture.width);
+        let y1 = p1.y * (h / capture.height);
+        let x2 = p2.x * (w / capture.width);
+        let y2 = p2.y * (h / capture.height);
+        line(x1, y1, x2, y2);
+      }
+    }
+
+    // 左眼內圈 (包含 246)：紅色，粗細 1
+    strokeWeight(1);
+    for (let i = 0; i < eyeInnerIndices.length; i++) {
+      let p1 = face.keypoints[eyeInnerIndices[i]];
+      let p2 = face.keypoints[eyeInnerIndices[(i + 1) % eyeInnerIndices.length]];
+
+      if (p1 && p2) {
         let x1 = p1.x * (w / capture.width);
         let y1 = p1.y * (h / capture.height);
         let x2 = p2.x * (w / capture.width);
